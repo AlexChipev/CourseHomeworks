c483d87e58d5b9d7409922931ef4ce6e861f201f
AlexChipev <alexchipev@abv.bg>
2015-06-10 21:43:50 +0300

    FirstConsole

---
+++ HelloWorld3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewWorld2
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Hello World! This is my first program!");
+            {
+                Console.WriteLine("Write on c#");
+                {
+                    Console.WriteLine("The End!!!"); 
+                }
+            }
+        }
+    }
+}
---
+++ MyFirstConsole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsole
+{
+    class MainClass
+    {
+        public static void Main (string [] args)
+        {
+            Console.WriteLine ("I`m Alex <Press enter to continue>");
+            Console.ReadKey();
+            Console.WriteLine("\n31 years old <Press space to continue>");
+            Console.ReadKey();
+            Console.WriteLine("\n Bye :-P");
+            Console.ReadKey();
+        }
+    }
+}
