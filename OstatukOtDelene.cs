d42eec00b90976a6ceeeddfd40d5de753822c82d
AlexChipev <alexchipev@abv.bg>
2015-06-15 22:01:23 +0300

    Homework2

---
+++ OstatukOtDelene.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 26;
+            int b = 8;
+            int result = a % b;
+            Console.WriteLine(result);
+        }
+    }
+}
