dcbad1b8459dd350d47d3da44f6731301a9c6801
AlexChipev <alexchipev@abv.bg>
2015-06-20 12:06:55 +0300

    Homework3

---
+++ Mazalo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a card: ");
+            int cardSymbol = int.Parse(Console.ReadLine());
+  
+                        switch (cardSymbol)
+            {
+                case 2:
+                    Console.WriteLine("Valid");
+                    break;
+                case 3:
+                    Console.WriteLine("Valid");
+                    break;
+                case 4:
+                    Console.WriteLine("Valid");
+                    break;
+                case 5:
+                    Console.WriteLine("Valid");
+                    break;
+                case 6:
+                    Console.WriteLine("Valid");
+                    break;
+                case 7:
+                    Console.WriteLine("Valid");
+                    break;
+                case 8:
+                    Console.WriteLine("Valid");
+                    break;
+                case 9:
+                    Console.WriteLine("Valid");
+                    break;
+                case 10:
+                    Console.WriteLine("Valid");
+                    break;
+                case 11:
+                    Console.WriteLine("Valid");
+                    break;
+
+                    string card = Console.ReadLine();
+                    switch (card)
+                    {
+                        case "j": Console.WriteLine("j, q, k, a"); break; 
+                        case "q":
+                        case "k":
+                        case "a":
+                            {
+                                Console.WriteLine("Valid");
+                                break;
+ 
+                                }
+                            }
+                    }
+                        }
+
+
+                        
+                        }
+
+                    }
+
+
+        
+
+
+
+
